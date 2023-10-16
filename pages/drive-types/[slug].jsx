1ea60ad37205ae96277e220cc803e61d92250922
Matan <matansaban28@gmail.com>
2023-10-16 06:00:35 +0300

    need to fix [slug].jsx

--- pages/drive-types/[slug].jsx
+++ pages/drive-types/[slug].jsx
@@ -4,39 +4,41 @@ import { useRouter } from 'next/router';
 import React from 'react'
 
 const DriveType = (props) => {
-    console.log('props of [slug]');
-    console.log(props);
-    const router = useRouter();
-    const { slug } = router.query;
+    // console.log('props of [slug]');
+    // console.log(props);
+    // const router = useRouter();
+    // const { slug } = router.query;
     return (
-        <div>{slug}</div>
+        <div>
+            {/* {slug} */}
+            </div>
     )
 }
 
 export default DriveType
 
-export async function getStaticPaths() {
-    const res = await axios.get(`${process.env.DATA_SOURCE}/transportation_types`, {
-        headers: {
-          Authorization: `${process.env.WORDPRESSTOKEN}`,
-        },
-      });
-    const driveTypes = res.data;
+// export async function getStaticPaths() {
+//     const res = await axios.get(`${process.env.DATA_SOURCE}/transportation_types`, {
+//         headers: {
+//           Authorization: `${process.env.WORDPRESSTOKEN}`,
+//         },
+//       });
+//     const driveTypes = res.data;
 
-    const paths = driveTypes.map((driverType) => ({
-        params: { slug: driverType.slug },
-    }));
+//     const paths = driveTypes.map((driverType) => ({
+//         params: { slug: driverType.slug },
+//     }));
 
-    return { paths, fallback: 'blocking' };
-}
+//     return { paths, fallback: 'blocking' };
+// }
 
-export async function getStaticProps({ params }) {
-    const res = await axios.get(`${process.env.DATA_SOURCE}/transportation_types?slug=${params.slug}`, {
-        headers: {
-          Authorization: `${process.env.WORDPRESSTOKEN}`,
-        },
-      });
-    const post = res.data[0];
+// export async function getStaticProps({ params }) {
+//     const res = await axios.get(`${process.env.DATA_SOURCE}/transportation_types?slug=${params.slug}`, {
+//         headers: {
+//           Authorization: `${process.env.WORDPRESSTOKEN}`,
+//         },
+//       });
+//     const post = res.data[0];
 
-    return { props: { post }, revalidate: 60 };
-}
+//     return { props: { post }, revalidate: 60 };
+// }
