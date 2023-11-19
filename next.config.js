eda65d1d8dc6fc715720dcfac9fe7442a1ca7185
MatanSaban <105128031+MatanSaban@users.noreply.github.com>
2023-11-19 11:31:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   //   buildActivity: false,
   // },
   env: {
-    GOOGLE_MAPS_API_KEY: "AIzaSyDqXMWSWoY417DNKERQid8teEuoxBjMLLo",
+    GOOGLE_MAPS_API_KEY: "AIzaSyA6Mm8uJAQNkzntDk0gQ6zt0tIom8zZAlQ",
     DATA_SOURCE: "https://saban-tours.ussl.co.il/wp-json/wp/v2",
     WORDPRESSTOKEN:
       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsIm5hbWUiOiJTdXBlckFkbWluU2FiYW4iLCJpYXQiOjE2OTg5NjY3MzAsImV4cCI6MTg1NjY0NjczMH0.JK2T-hU0Ij3DgNuXSG9zxWRlntMfxfUiy2f1fXk5ryo",
